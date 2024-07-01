619319c9a4433e5576f749fdf4296addf4795fa7
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-07-01 23:50:35 +0200

    version

--- src/test/java/day01_API/C01_Get_ResponseBodyYazdirma.java
+++ src/test/java/day01_API/C01_Get_ResponseBodyYazdirma.java
@@ -1,7 +1,15 @@
 package day01_API;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+
 
 public class C01_Get_ResponseBodyYazdirma {
 
+
+    @Test
     public void get01(){
 
         // manuel olarak postman de kontrol edildi
@@ -10,9 +18,21 @@ public class C01_Get_ResponseBodyYazdirma {
 
         // 1- Request body ve end-point hazirlama
 
+        String Url = "https://restful-booker.herokuapp.com/booking/10";
+
+        // 2- Expected data hazirlama
+
+        // 3- Request gonderip, donen response'i kaydetme
+
+        Response response = given().when().get(Url);
+        response.prettyPrint();
+
+        // 4- Assertion
+
+        //Postman daki responce un aynısınıı getirdi !!
 
 
+    }
 
-       }
 
     }
