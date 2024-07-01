07ab59c467b068c86d6bf8fdbe8a4b11938fd694
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-07-01 23:38:08 +0200

    first commit

---
+++ src/test/java/day01_API/C01_Get_ResponseBodyYazdirma.java
@@ -0,0 +1,18 @@
+package day01_API;
+
+public class C01_Get_ResponseBodyYazdirma {
+
+    public void get01(){
+
+        // manuel olarak postman de kontrol edildi
+        // https://restful-booker.herokuapp.com/booking/10 url’ine
+        // bir GET request gonderdigimizde donen Response’u yazdirin.
+
+        // 1- Request body ve end-point hazirlama
+
+
+
+
+       }
+
+    }
