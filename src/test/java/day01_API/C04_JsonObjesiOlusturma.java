9cff9df8cf119ffd498e069c7ae702a9c4b18954
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-07-02 15:56:26 +0200

    version

--- src/test/java/day01_API/C04_JsonObjesiOlusturma.java
+++ src/test/java/day01_API/C04_JsonObjesiOlusturma.java
@@ -1,4 +1,33 @@
 package day01_API;
 
+import netscape.javascript.JSObject;
+import org.json.JSONObject;
+import org.junit.Test;
+
 public class C04_JsonObjesiOlusturma {
+
+    @Test
+    public void test02(){
+        /*
+            {
+            "title":"Ahmet",
+            "body":"Merhaba",
+            "userId":1
+            }
+         */
+
+        JSONObject object = new JSONObject();
+        object.put("title","Ahmet");
+        object.put("body","Merhaba");
+        object.put("userId",1);
+
+        System.out.println(object);
+
+        //{"title":"Ahmet","body":"Merhaba","userId":1}
+
+
+
+
+
+    }
 }
