eb23367661328931ed2385f4c67e93c33b99a1f4
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-07-02 21:41:18 +0200

    version

---
+++ src/test/java/day01_API/C07_Get_ResponseBodyTesti.java
@@ -0,0 +1,26 @@
+package day01_API;
+
+import org.junit.Test;
+
+public class C07_Get_ResponseBodyTesti {
+
+
+    @Test
+    public void test01(){
+        /*
+        https://jsonplaceholder.typicode.com/posts/44 url'ine bir GET request yolladigimizda
+        donen Response’in
+        	status code'unun 200,
+        	ve content type'inin Aplication.JSON,
+        	ve response body'sinde bulunan userId'nin 5,
+        	ve response body'sinde bulunan title'in "optio dolor molestias sit"
+        	oldugunu test edin.
+
+         */
+
+
+
+
+    }
+
+}
