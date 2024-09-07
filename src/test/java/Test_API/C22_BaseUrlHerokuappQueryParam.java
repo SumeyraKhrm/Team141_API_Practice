3a1b806deeff7c010b7d17a22f2c65d03df11083
SumeyraKhrm <sumeyra.khrm@outlook.com>
2024-09-07 23:36:45 +0200

    version

---
+++ src/test/java/Test_API/C22_BaseUrlHerokuappQueryParam.java
@@ -0,0 +1,29 @@
+package Test_API;
+
+import org.junit.Test;
+
+public class C22_BaseUrlHerokuappQueryParam {
+
+
+    @Test
+    public void test01() {
+        /*
+
+            https://restful-booker.herokuapp.com/booking endpointine
+            gerekli Query parametrelerini yazarak
+                “firstname” degeri “Susan”
+                ve “lastname” degeri “Ericson” olan
+            rezervasyon oldugunu test edecek bir GET request gonderdigimizde,
+            donen response’un
+                status code’unun 200 oldugunu
+                ve “Eric Jones” ismine sahip en az bir booking oldugunu test edin
+         */
+
+        // 1- endpoint ve request body olustur
+
+
+
+
+
+    }
+}
